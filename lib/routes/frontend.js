nicevibesplus/ErstiWeToolDocker

commit da45ab3b0bbe9f46d06dc41b987c86e6a9a76792

WIP add attendee list print view

lib/routes/frontend.js
@@ -7,7 +7,9 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/admin', auth, function(req, res){
+router.use(auth)
+
+router.get('/admin', function(req, res){
   db.getYears((err, years) => {
     const locals = {
       year: req.query.year || cfg.year,
@@ -17,10 +19,13 @@ router.get('/admin', auth, function(req, res){
   })
 });
 
-router.get('/admin/print/:aspect', auth, function(req, res){
-  res.render(`admin_print_${req.params.aspect}`, {
-    year: req.query.year || cfg.year
-  });
+router.get('/admin/print/:aspect', function(req, res){
+  db.getAttendees((err, attendees) => {
+    res.render(`admin_print_${req.params.aspect}`, {
+      attendees,
+      year: req.query.year || cfg.year,
+    })
+  })
 })
 
 module.exports = router;

lib/views/admin.pug
@@ -111,5 +111,6 @@ html
 
     script(src='./js/jquery-2.2.4.min.js' integrity='sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' crossorigin='anonymous')
     script(src="./js/jquery.dataTables.js")
-    script(src="./js/Chart.min.js")
     script(src='./js/admin.js')
+    script(src="./js/Chart.min.js")
+    script(src='./js/admin-charts.js')

lib/views/admin_print_attending.pug
@@ -1,0 +1,42 @@
+doctype html
+html
+  head
+    meta(http-equiv='content-type', content='text/html; charset=utf-8; width=device-width; initial-scale=1.0')
+    style.
+      body {
+        margin: 0;
+        font-family: sans;
+      }
+      table tr:nth-child(odd) {
+        background-color: #ccc;
+      }
+      @media print {
+        .status { display: none; }
+      }
+
+  body
+    h3= 'Erstifahrt ' + year + ': ' + attendees.length + ' Teilnehmende'
+
+    div
+      canvas#chart-gender(width="300", height="200")
+      canvas#chart-study(width="400", height="200")
+      canvas#chart-food(width="400", height="200")
+
+    table.table
+      thead
+        tr
+          th Name
+          th Geburtstag
+          th Studiengang
+          th Kommentar
+
+      each u in attendees
+        tr
+          td= u.firstname + ' ' + u.lastname
+          td= u.birthday
+          td= u.study
+          td= u.comment
+
+    script(src='../../js/jquery-2.2.4.min.js' integrity='sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' crossorigin='anonymous')
+    script(src="../../js/Chart.min.js")
+    script(src='../../js/admin-charts.js')

public/js/admin-charts.js
@@ -1,0 +1,60 @@
+'use strict';
+
+/* global $ Chart */
+
+$(document).ready(function() {
+  // get year from url query to apply to all backend requests
+  const urlQuery = window.location.search
+    .slice(1) // remove leading '?'
+    .split('&') // split params
+    .reduce((result, kv) => { // make an object of key value pairs
+      const [key, value] = kv.split('=');
+      return Object.assign(result, { [key]: value });
+    }, {});
+
+    const yearParam = urlQuery.year ? `?year=${urlQuery.year}` : '';
+
+  // pie chart stats
+  const COLORS = {
+    'Geographie': 'orange',
+    'Geoinformatik': 'cornflowerblue', // uuuuh
+    'Landschaftsökologie': 'mediumseagreen',
+    'Zwei-Fach-Bachelor': 'gold',
+
+    'female': 'orange',
+    'male': 'gold',
+    'other': 'grey',
+
+    'fleischig': 'darkred',
+    'vegan': 'olive',
+    'vegetarisch': 'yellowgreen',
+  };
+
+  function getColors (stats) {
+    return stats.map(s => COLORS[s.field]);
+  }
+
+  ['gender', 'study', 'food'].forEach(function(aspect) {
+    $.get(`./api/statistics/${aspect}${yearParam}`, function(stats, status) {
+      if (status !== 'success') return console.error(stats, 'error');
+
+      var canvas = document.getElementById("chart-"+aspect);
+      var options = {
+        type: 'pie',
+        data: {
+          labels: stats.map(s => s.field),
+          datasets: [{
+            data: stats.map(s => s.count),
+            backgroundColor: getColors(stats),
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: false,
+          maintainAspectRation: false
+        }
+      };
+      new Chart(canvas, options);
+    });
+  });
+});

public/js/admin.js
@@ -40,50 +40,6 @@ $(document).ready(function() {
       $('#count-' + measure).html(stats[measure]);
   });
 
-  // pie chart stats
-  const COLORS = {
-    'Geographie': 'orange',
-    'Geoinformatik': 'cornflowerblue', // uuuuh
-    'Landschaftsökologie': 'mediumseagreen',
-    'Zwei-Fach-Bachelor': 'gold',
-
-    'female': 'orange',
-    'male': 'gold',
-    'other': 'grey',
-
-    'fleischig': 'darkred',
-    'vegan': 'olive',
-    'vegetarisch': 'yellowgreen',
-  };
-
-  function getColors (stats) {
-    return stats.map(s => COLORS[s.field]);
-  }
-
-  ['gender', 'study', 'food'].forEach(function(aspect) {
-    $.get(`./api/statistics/${aspect}${yearParam}`, function(stats, status) {
-      if (status !== 'success') return console.error(stats, 'error');
-
-      var canvas = document.getElementById("chart-"+aspect);
-      var options = {
-        type: 'pie',
-        data: {
-          labels: stats.map(s => s.field),
-          datasets: [{
-            data: stats.map(s => s.count),
-            backgroundColor: getColors(stats),
-            borderWidth: 1
-          }]
-        },
-        options: {
-          responsive: false,
-          maintainAspectRation: false
-        }
-      };
-      new Chart(canvas, options);
-    });
-  });
-
   // fill user table
   $.get(`./api/users${yearParam}`, function(users, status) {
     if (status !== 'success') return console.error(users, 'error');

commit 0b50389db4826c82e934d79c85a8ed1bc5276d87

fix #18
regression from da45ab3

lib/routes/frontend.js
@@ -7,9 +7,7 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.use(auth)
-
-router.get('/admin', function(req, res){
+router.get('/admin', auth, function(req, res){
   db.getYears((err, years) => {
     const locals = {
       year: req.query.year || cfg.year,
@@ -19,7 +17,7 @@ router.get('/admin', function(req, res){
   })
 });
 
-router.get('/admin/print/:aspect', function(req, res){
+router.get('/admin/print/:aspect', auth, function(req, res){
   const year = req.query.year || cfg.year
   db.getAttendees(year, (err, attendees) => {
     res.render(`admin_print_${req.params.aspect}`, { attendees, year })